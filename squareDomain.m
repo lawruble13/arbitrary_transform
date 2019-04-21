f7f5e5c76bcb709aac1ae4f5d61308a4edd0db93
Liam Wrubleski <liam@wrubleski.net>
2019-04-21 14:27:20 -0600

    Sped up by changing loop logic, working on ITU_R_468 human hearing curve

--- squareDomain.m
+++ squareDomain.m
@@ -1,12 +1,13 @@
+ITU_R;
 close all;
 
 recent = datetime('now');
 start = recent;
 
-S_FREQ = 96000;
-MAX = 100;
-RUN_TIME = 2;
-W_FREQ = 1000;
+S_FREQ = 44100;
+MAX = 50;
+RUN_TIME = 1;
+W_FREQ = 100;
 L = RUN_TIME*S_FREQ;
 
 prime_arr = primes(MAX);
@@ -14,13 +15,10 @@ prime_arr = prime_arr(2:end);
 
 t = 1/S_FREQ * (0:L-1);
 squareWaves = containers.Map('KeyType','double','ValueType','double');
-S = ((pi/2)*square(2*pi*W_FREQ*t));
-squareWaves(W_FREQ) = pi/2;
+squareWaves(W_FREQ) = pi/4;
 used = NaN(size(prime_arr));
 active = 1;
 curmax = length(prime_arr);
-figure();
-hold on;
 i=0;
 while true
     count = 0;
@@ -56,36 +54,53 @@ while true
         break;
     end
     active = prod(prime_arr.*used,'omitnan');
-    if(active > S_FREQ*10)
+    if(active > S_FREQ*2)
         curmax = sum(used, 'omitnan');
         continue;
     end
-    %S = S + ((-1)^(sum(used, 'omitnan')))*(pi/2)*square(2*pi*W_FREQ*t*active)/active; 
     if(isKey(squareWaves,active*W_FREQ))
-        squareWaves(active*W_FREQ) = squareWaves(active*W_FREQ)+((-1)^(sum(used, 'omitnan')))*(pi/2)/active;
+        squareWaves(active*W_FREQ) = squareWaves(active*W_FREQ)+((-1)^(sum(used, 'omitnan')))*(pi/4)/active;
     else
-        squareWaves(active*W_FREQ) = ((-1)^(sum(used, 'omitnan')))*(pi/2)/active;
+        squareWaves(active*W_FREQ) = ((-1)^(sum(used, 'omitnan')))*(pi/4)/active;
     end
     i=0;
 end
+S = zeros(size(t));
+for k = cell2mat(keys(squareWaves))
+    %if(k<30000)
+        S = S+squareWaves(k)*square(2*pi*k*t);
+    %end
+end
+td = figure();
+movegui(td, 'west');
 hold on;
+sound(S, S_FREQ);
 plot(t, S,'DisplayName','Approximation');
 plot(t, sin(2*pi*W_FREQ*t), 'DisplayName', 'Actual');
-for k = cell2mat(keys(squareWaves))
-    if(k<30000)
-        sound(squareWaves(k)*square(2*pi*k*t), S_FREQ);
-    end
-end
 title("Sine wave approximation using square waves");
-figure;
-hold off;
+fd = figure();
+movegui(fd, 'east');
 T = fft(S);
 P2 = abs(T/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
-
 f = S_FREQ*(0:(L/2))/L;
-plot(f, mag2db(abs(P1)));
-title("Single-Sided Amplitude Spectrum of sine wave approximation")
+P1d = mag2db(abs(P1));
+plot(f, P1d,'DisplayName','Unfiltered');
+hold on;
+for i = 1:length(f)
+    j = 1;
+    while ~(ITU_R_468(j,1) < f(i) && ITU_R_468(j+1,1) > f(i)) && j < length(ITU_R_468)
+        j=j+1;
+    end
+    if j == length(ITU_R_468)
+        P1d(i) = P1d(i) + ITU_R_468(j,2);
+    else
+        P1d(i) = P1d(i) + (f(i)-ITU_R_468(j,1)) * (ITU_R_468(j+1,2)-ITU_R_468(j,2)) / (ITU_R_468(j+1,1)-ITU_R_468(j,1));
+    end
+end
+plot(f, P1d,'DisplayName','Filtered by human hearing');
+title("Single-Sided Amplitude Spectrum of sine wave approximation");
+hold off;
 
 

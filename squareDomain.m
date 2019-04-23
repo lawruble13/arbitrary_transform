30d3e66aea6862854d64b5ec462f2ebf2f0c074d
Liam Wrubleski <liam@wrubleski.net>
2019-04-23 15:24:41 -0600

    Sped up, added capability to display in dB or magnitude

--- squareDomain.m
+++ squareDomain.m
@@ -1,3 +1,4 @@
+digitsOld = digits(100);
 ITU_R;
 close all;
 
@@ -5,15 +6,16 @@ recent = datetime('now');
 start = recent;
 
 S_FREQ = 44100;
-MAX = 50;
-RUN_TIME = 1;
-W_FREQ = 100;
+MAX = 200;
+RUN_TIME = 10/6300;
+W_FREQ = 6300;
 L = RUN_TIME*S_FREQ;
+F_PLOT_MODE = 'mag';
 
 prime_arr = primes(MAX);
-prime_arr = prime_arr(2:end);
+prime_arr = vpa(prime_arr(2:end));
 
-t = 1/S_FREQ * (0:L-1);
+t = vpa(1)/S_FREQ * (0:L-1);
 squareWaves = containers.Map('KeyType','double','ValueType','double');
 squareWaves(W_FREQ) = pi/4;
 used = NaN(size(prime_arr));
@@ -85,22 +87,39 @@ P2 = abs(T/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 f = S_FREQ*(0:(L/2))/L;
-P1d = mag2db(abs(P1));
+if strcmp(F_PLOT_MODE, 'mag')
+    P1d = P1;
+elseif strcmp(F_PLOT_MODE, 'db')
+    P1d = mag2db(abs(P1));
+end
 plot(f, P1d,'DisplayName','Unfiltered');
 hold on;
+HHTF = zeros(size(f));
 for i = 1:length(f)
-    j = 1;
-    while ~(ITU_R_468(j,1) < f(i) && ITU_R_468(j+1,1) > f(i)) && j < length(ITU_R_468)
+    j = 2;
+    FL = ITU_R_468(j-1,1);
+    FR = ITU_R_468(j,1);
+    RL = ITU_R_468(j-1,2);
+    RR = ITU_R_468(j,2);
+    while (FR < f(i)) && j < length(ITU_R_468)
         j=j+1;
+        FL = ITU_R_468(j-1,1);
+        FR = ITU_R_468(j,1);
+        RL = ITU_R_468(j-1,2);
+        RR = ITU_R_468(j,2);
     end
-    if j == length(ITU_R_468)
-        P1d(i) = P1d(i) + ITU_R_468(j,2);
-    else
-        P1d(i) = P1d(i) + (f(i)-ITU_R_468(j,1)) * (ITU_R_468(j+1,2)-ITU_R_468(j,2)) / (ITU_R_468(j+1,1)-ITU_R_468(j,1));
+    if strcmp(F_PLOT_MODE, 'mag')
+        HHTF(i) = db2mag((f(i)-FL) * (RR-RL) / (FR-FL) + RL);
+        P1d(i) = P1d(i) * HHTF(i);
+    elseif strcmp(F_PLOT_MODE, 'db')
+        HHTF(i) = (f(i)-FL) * (RR-RL) / (FR-FL) + RL;
+        P1d(i) = P1d(i) + HHTF(i);
     end
 end
-plot(f, P1d,'DisplayName','Filtered by human hearing');
+%plot(f, P1d,'DisplayName','Filtered by human hearing');
+%plot(f, HHTF, 'DisplayName','Human hearing transfer function');
 title("Single-Sided Amplitude Spectrum of sine wave approximation");
 hold off;
+digits(digitsOld);
 
 
